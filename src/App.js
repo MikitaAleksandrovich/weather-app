f226da2b4f5ce2d1cef8adce644ff0c44a865353
Mikita Aleksandrovich <45469739+MikitaAleksandrovich@users.noreply.github.com>
2019-12-22 13:48:19 +0300

    Create Current weather component and display weather based on inputted city

--- src/App.js
+++ src/App.js
@@ -2,22 +2,21 @@ import React, {Component} from 'react';
 import './App.css';
 import Header from './components/header/header';
 import SearchBar from './components/search-bar/search_bar';
+import CurrentWeather from './components/current-weather/current-weather';
 import {bindActionCreators} from 'redux';
 import {fetchWeather} from './actions/index';
 import {connect} from 'react-redux';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      city: ''
-    }
-  };
+  state = {
+    city: '',
+    cityArr: [],
+  }
 
   onInputChange = (e)=> {
     this.setState({
       city: e.target.value
-    })
+    });
   };
 
 
@@ -27,6 +26,9 @@ class App extends Component {
     this.setState({
       city: '',
     });  
+    this.state.cityArr.push(this.state.city);
+    localStorage.setItem('cities', JSON.stringify(this.state.cityArr));
+    console.log(this.state.cityArr);
   };
 
 
@@ -43,6 +45,7 @@ class App extends Component {
                 onInputChange = {this.onInputChange}
                 city = {city}
               />
+              <CurrentWeather city={city}/>
             </div>        
           </section>
         );
@@ -59,6 +62,7 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => {
   return bindActionCreators({fetchWeather}, dispatch);
 };
+
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
 
--- src/components/search-bar/search_bar.js
+++ src/components/search-bar/search_bar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './search_bar.css';
 
+
 class SearchBar extends React.Component{
 
   render() {
@@ -9,26 +10,29 @@ class SearchBar extends React.Component{
       <section>
           <div className="row d-flex justify-content-center">
             <div className="col-md-8 ">
-              <form onSubmit={e => this.props.onFormSubmit(e)}>
+              <form onSubmit={e => this.props.onFormSubmit(e)} autoComplete="on">
                 <div className="input-group input-container">
                   <input
                      type="text"
                      className="form-control"
+                     
                      placeholder="Enter City Name"
                      value={this.props.city}
                      onChange={this.props.onInputChange}
                     />
                   <div className="input-group-prepend search">
-                    <i className="fa fa-search" onClick={e=>this.props.onFormSubmit(e)}></i>
+                    <i className="fa fa-search"></i>
                   </div>
+                  
                 </div>
               </form>
             </div>
-            </div>
-        </section>
+          </div>
+      </section>   
     )
   }
 };
 
 
 export default SearchBar;
+
